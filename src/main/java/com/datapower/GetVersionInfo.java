13ae7dd182cb25b1c1d6cf6b1afd84010b1478f2
Conor Griffin <conorgriffin@gmail.com>
2015-05-09 13:18:02 +0100

    Initial Commit

---
+++ src/main/java/com/datapower/GetVersionInfo.java
@@ -0,0 +1,8 @@
+package com.datapower;
+
+public class GetVersionInfo {
+	public static void main(String[] args) {
+		//TODO: update the version string below to match the DataPower firmware version the WSDL was taken from
+		System.out.println("Compiled from DataPower firmware version 7.1.0.4");
+	}
+}
