096feb3c3e642562bce3db0417807931bce4bc20
Conor Griffin <conorgriffin@gmail.com>
2015-05-11 09:34:03 +0100

    No longer need to change versions in two places.  Updating version in pom.xml is enough

--- src/main/java/com/datapower/GetVersionInfo.java
+++ src/main/java/com/datapower/GetVersionInfo.java
@@ -1,8 +1,35 @@
 package com.datapower;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class GetVersionInfo {
-    public static void main(String[] args) {
+	
+    public static void main(String[] args) throws IOException {
         //TODO: update the version string below to match the DataPower firmware version the WSDL was taken from
-        System.out.println("Compiled from DataPower firmware version 7.1.0.4");
+    	Properties prop = new Properties();
+    	InputStream input = null;
+    	try {
+    		input = GetVersionInfo.class.getClassLoader().getResourceAsStream("version.properties");
+    		if(input == null) {
+    			System.out.println("Version file does not exist.");
+    		}
+    		
+    		prop.load(input);
+    		String versionString = prop.getProperty("datapower.firmware.version");
+            System.out.println("Compiled from DataPower firmware version " + versionString);
+
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	} finally {
+    		if (input != null) {
+    			try {
+        			input.close();    				
+    			} catch (IOException e) {
+    				e.printStackTrace();
+    			}
+    		}
+    	}
     }
 }
